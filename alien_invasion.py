36251709b75fa5b011054ada7e4d0c5d02a0f2dc
Erin Pronobis <erinpronobis@Erins-MacBook-Air.local>
2022-09-07 07:08:07 -0400

    creation of alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -21,6 +22,14 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
+
+    def _create_fleet(self):
+        """Create the fleet of aliens"""
+        # Make an Alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
 
 
     def run_game(self):
@@ -82,6 +91,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
